4ae727ff7e6d979e31af84373245ee448f8d27b2
Paul Dela Bueno <pjaydlbn38@gmail.com>
2026-03-20 19:37:09 +0800

    structure

---
+++ test.py
@@ -0,0 +1,16 @@
+import os
+
+folders = [
+    "data", "data/raw", "data/processed", "data/splits",
+    "notebooks",
+    "src", "src/cnn", "src/nlp", "src/rl",
+    "experiments",
+    "outputs", "outputs/models", "outputs/plots",
+    "docs"
+]
+
+for folder in folders:
+    os.makedirs(folder, exist_ok=True)
+    open(os.path.join(folder, ".gitkeep"), "w").close()
+
+print("Folders + .gitkeep files created!")
